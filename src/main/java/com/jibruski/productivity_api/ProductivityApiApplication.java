f19047d37128fdd7108da3f4e4f4a2e19d3b2807
n-abrij <omollojibran@gmail.com>
2026-07-26 18:28:46 +0300

    Initial Commit

---
+++ src/main/java/com/jibruski/productivity_api/ProductivityApiApplication.java
@@ -0,0 +1,13 @@
+package com.jibruski.productivity_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductivityApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductivityApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jibruski/productivity_api/ProductivityApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jibruski.productivity_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductivityApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
