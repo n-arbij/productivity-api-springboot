c6146aec992123537c970f5cfb71af766a641712
n-abrij <omollojibran@gmail.com>
2026-07-28 09:44:18 +0300

    fix: Request Matcher

--- src/main/java/com/jibruski/productivity_api/security/SecurityConfig.java
+++ src/main/java/com/jibruski/productivity_api/security/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig {
             .csrf(csrf -> csrf.disable())
             .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/api/auth/**").permitAll()
+                .requestMatchers("/auth/**").permitAll()
                 .anyRequest().authenticated())
             .exceptionHandling(ex -> ex
                 .authenticationEntryPoint(new JwtAuthExceptionHandler.EntryPoint())
